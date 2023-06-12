1e19ea628ec872a551fdcf8ade8ad7b888c7b771
Julio Mojica <juliocmojica@gmail.com>
2023-06-11 20:39:36 -0400

    init typescript config and eslint

--- src/routers/restaurant.router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {getRestaurantsNearZipcode} from '../controllers/restaurant.controller';
-
-const restaurantRouter = express.Router();
-
-restaurantRouter.get('/near/:zipcode', getRestaurantsNearZipcode);
-
-export default restaurantRouter;
