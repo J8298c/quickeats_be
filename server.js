96db20d0ed6036c00298563280c00112d9467add
Julio Mojica <juliocmojica@gmail.com>
2023-06-12 06:16:35 -0400

    add router to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const restaurantRouter = require('./controllers/restaurant.controller');
 
 dotenv.config();
 
@@ -18,5 +19,6 @@ mongoose
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use('/api/restaurants', restaurantRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
